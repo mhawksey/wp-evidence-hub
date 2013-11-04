3a43c57f7c486af1d63c58f587a2836cad89087a
Nick Freear <nfreear@yahoo.co.uk>
2013-11-04 17:17:38 +0000

    Handle symbolic links to plugin code - code portability..
    * define(EVIDENCE_HUB_URL, ..) define(EVIDENCE_HUB_REGISTER_FILE, ..)

--- wp_evidence_hub.php
+++ wp_evidence_hub.php
@@ -33,8 +33,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 define('EVIDENCE_HUB_VERSION', '0.3');
 define('EVIDENCE_HUB_PATH', dirname(__FILE__));
-define('EVIDENCE_HUB_URL', plugin_dir_url(__FILE__));
-define('EVIDENCE_HUB_REGISTER_FILE', __FILE__);
+// Handle symbolic links - code portability.
+define('EVIDENCE_HUB_URL', plugin_dir_url(preg_replace('@\/var\/www\/[^\/]+@', '', __FILE__)));
+define('EVIDENCE_HUB_REGISTER_FILE', preg_replace('@\/var\/www\/[^\/]+@', '', __FILE__));
 
 
 
