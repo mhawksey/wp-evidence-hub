0a78949f986ed6ed251f81149589f23d0eaa5231
Nick Freear <nfreear@yahoo.co.uk>
2015-05-07 11:12:10 +0100

    Bug #41, Fix `_REGISTER_FILE` path for plugin [iet:2349318] [iet:3412000]
    * Part of today's deployment/ switch to `Composer`-based deploy

--- wp_evidence_hub.php
+++ wp_evidence_hub.php
@@ -28,8 +28,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 define('EVIDENCE_HUB_VERSION', '0.1.1');
 define('EVIDENCE_HUB_PATH', dirname(__FILE__));
 // Handle symbolic links - code portability.
-define('EVIDENCE_HUB_URL', plugin_dir_url(preg_replace('@\/var\/www\/[^\/]+@', '', __FILE__)));
-define('EVIDENCE_HUB_REGISTER_FILE', preg_replace('@\/var\/www\/[^\/]+@', '', __FILE__));
+define('EVIDENCE_HUB_URL', plugin_dir_url(preg_replace('@^.*wp-content\/plugins@', '', __FILE__)));
+define('EVIDENCE_HUB_REGISTER_FILE', __FILE__);  //preg_replace('@^.*wp-content\/plugins@', '', __FILE__));
 
 
 require_once 'php/evidence_hub_base.php';
